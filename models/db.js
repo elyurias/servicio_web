fa890a8f73cd5e01b02f1ac740be0368e05d277a
elyurias <irving2323@gmail.com>
2018-06-26 22:19:43 -0500

    La Sabiduria de Salomon

--- models/db.js
+++ models/db.js
@@ -1,2 +1,3 @@
 ﻿var mongoose = require('mongoose');
-mongoose.connect('mongodb://irvingk:alir4101@ds123718.mlab.com:23718/web_service');
+//mongoose.connect('mongodb://irvingk:alir4101@ds123718.mlab.com:23718/web_service');
+mongoose.connect('mongodb://irvingk:alir4101@localhost:27017/web_service');
